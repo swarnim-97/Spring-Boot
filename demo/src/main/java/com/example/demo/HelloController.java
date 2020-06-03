799e7629351647deec36f2bce3182ad5e936a691
swarnim-97 <swarnimpradhan1997@gmail.com>
2020-06-03 20:25:35 +0530

    Spring serving Web content

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
     @RequestMapping("/")
     public String index(){
-        return "Go Corona Go";
+        return "main-menu";
     }
 }
